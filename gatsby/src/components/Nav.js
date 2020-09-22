c3a61010255b192f5f1bdeb65b1d6099615ba776
Nick McCullum <nicholasmccullum@gmail.com>
2020-09-22 09:34:02 -0300

    finished nav layout and built pizza schema

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -17,6 +17,7 @@ const NavStyles = styled.nav`
     grid-template-columns: 1fr 1fr auto 1fr 1fr;
     grid-gap: 2rem;
     align-items: center;
+    margin-top: -6rem;
   }
   li {
     --rotate: -2deg;
@@ -40,6 +41,9 @@ const NavStyles = styled.nav`
     text-decoration: none;
     display: block;
     &:hover {
+      color: var(--red);
+    }
+    &[aria-current='page'] {
       color: var(--red);
     }
   }
