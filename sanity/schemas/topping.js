78ced34e181b142a7696aff56ec7ffc5e6f868de
Nick McCullum <nicholasmccullum@gmail.com>
2020-09-22 09:43:52 -0300

    created toppings data type

---
+++ sanity/schemas/topping.js
@@ -0,0 +1,32 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  // Computer name
+  name: 'topping',
+  // Visible title
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Topping',
+      type: 'string',
+      description: 'Name of the topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: (fields) => ({
+      title: `${fields.name} ${fields.vegetarian ? `(VEG)` : ``}`,
+    }),
+  },
+};
