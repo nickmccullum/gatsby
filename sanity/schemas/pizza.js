5d9543ac0123155da5d4357de2cfe3b724df0070
Nick McCullum <nicholasmccullum@gmail.com>
2020-09-22 09:59:20 -0300

    finalized pizza config

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,9 +1,10 @@
 import { MdLocalPizza as icon } from 'react-icons/md';
+import PriceInput from '../components/PriceInput';
 
 export default {
-  // Computer name
+  // Computer Name
   name: 'pizza',
-  // Visible title
+  // visible title
   title: 'Pizzas',
   type: 'document',
   icon,
@@ -36,7 +37,34 @@ export default {
       title: 'Price',
       type: 'number',
       description: 'Price of the pizza in cents',
-      validation: (Rule) => Rule.min(1000).max(50000),
+      validation: (Rule) => Rule.min(1000),
+      inputComponent: PriceInput,
+    },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. Filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean);
+      // 2. return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: tops.join(', '),
+      };
+    },
+  },
 };
