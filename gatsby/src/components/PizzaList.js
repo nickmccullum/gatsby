bb60c6b137a95a7401c32c496f6da5907f8f3536
Nick McCullum <nicholasmccullum@gmail.com>
2020-09-24 10:35:05 -0300

    got toppings to show below each individual pizza

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -4,7 +4,10 @@ import { Link } from 'gatsby';
 function SinglePizza({ pizza }) {
   return (
     <Link to={`/pizza/${pizza.slug.current}`}>
-      <h2>{pizza.name}</h2>
+      <h2>
+        <span className="mark">{pizza.name}</span>
+      </h2>
+      <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
     </Link>
   );
 }
