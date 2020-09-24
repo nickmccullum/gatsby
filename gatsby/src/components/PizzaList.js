450074b816fc0d464b399d7b0233620d6a0b9ce9
Nick McCullum <nicholasmccullum@gmail.com>
2020-09-24 10:07:16 -0300

    created PizzaList component and imported it into the pizzas.js page

---
+++ gatsby/src/components/PizzaList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function PizzaList() {
+  return <p>LIST OF PIZZAS</p>;
+}
--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
 export default function PizzasPage({ data }) {
   return (
     <>
+      <PizzaList />
       <p>Hey! There are {data.pizzas.nodes.length} pizzas available.</p>
     </>
   );
